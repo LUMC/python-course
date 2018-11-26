6383bfaaafd5533fc4ddf4a4a5a4f44c78d6c11f
Mihai <M.Lefter@lumc.nl>
2018-11-26 21:56:04 +0100

    Started day2 tex

---
+++ more_python/more_01/seq_toolbox.py
@@ -0,0 +1,13 @@
+def calc_gc_percent(seq):
+    at_count, gc_count = 0, 0
+    for char in seq:
+        if char in ('A', 'T'):
+            at_count += 1
+        elif char in ('G', 'C'):
+            gc_count += 1
+
+    return gc_count * 100.0 / (gc_count + at_count)       
+
+print("The sequence 'CAGG' has a %GC of {:.2f}".format(
+          calc_gc_percent("CAGG")))
+
